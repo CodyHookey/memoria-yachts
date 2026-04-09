f621286be70446056b419eba3f0996e787199945
Cody Hookey <149050408+CodyHookey@users.noreply.github.com>
2026-04-09 19:37:09 +0100

    SEO and robots.txt added

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -15,7 +15,27 @@ const Home = () => {
   return (
     <>
       <Helmet>
-        <title>Luxury Yacht Hire Dubai | Memoría Yachts</title>
+        <title>Luxury Yacht Charter | Memoría Yachts</title>
+
+        <meta
+          name="description"
+          content="Discover bespoke luxury yacht charters with Memoría Yachts. Explore exceptional vessels, curated destinations, and seamless experiences at sea."
+        />
+
+        <meta
+          property="og:title"
+          content="Luxury Yacht Charter | Memoría Yachts"
+        />
+        <meta
+          property="og:description"
+          content="Experience refined yachting with bespoke charters, world-class yachts, and curated journeys across the globe."
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://yourwebsite.com" />
+        <meta
+          property="og:image"
+          content="https://memoria-yachts.netlify.app/memoria-logo.png"
+        ></meta>
       </Helmet>
       <Navbar />
       <Hero />
